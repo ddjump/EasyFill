cc0ed1369ecff1e580f1d8c5b7f39fee8e641c01
ddjump <dyen576@gmail.com>
2018-01-27 13:12:31 -0500

    Initial Commit

---
+++ EasyFill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyFill
+//
+//  Created by Daniel Yen on 1/27/18.
+//  Copyright © 2018 Daniel Yen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
